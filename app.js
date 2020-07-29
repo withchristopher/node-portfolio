e3a6b3de47567d310ff7f74fc1fcd11dcd7bec00
Chris Maxwell <chmaxwel@icloud.com>
2020-07-29 18:40:27 +0200

    arrow functions

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
 var profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+    name: "Lernantino",
+    age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+}
+
+printProfileData(profileDataArgs);
+
+// Using function expression syntax
+const addNums = function(numOne, numTwo) {
+    return numOne + numTwo;
+};
+
+// Using new arrow function syntax
+const addNums = (numOne, numTwo) => {
+    return numOne + numTwo;
+};
+
+// One parameter, omitting the parentheses
+const printProfileData = profileDataArr => {
+    console.log(profileDataArr);
+};
+
+const addNums = (numOne, numTwo) => numOne + numTwo;
+
+const sum = addNums(5, 3); // add sum
