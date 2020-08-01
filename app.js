26cab64a8ad754e866b11536990ac1bebd657903
Chris Maxwell <chmaxwel@icloud.com>
2020-08-01 13:19:55 +0200

    exported generate-file.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([{
@@ -124,19 +124,24 @@ const promptProject = portfolioData => {
         });
 };
 
-
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('index.html', generatePage(name, github), err => {
-            if (err) throw new Error(err);
-
-            console.log('Portfolio Complete! Check out index.html to see the output');
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-
 
 // const profileDataArgs = process.argv.slice(2);
 
