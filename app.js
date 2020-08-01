5079b8a03955b73842d1a4a95e0ced532480f049
Chris Maxwell <chmaxwel@icloud.com>
2020-08-01 09:00:38 +0200

    added inquirer npm

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const inquirer = require('inquirer');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const profileDataArgs = process.argv.slice(2);
 
-    console.log('Portfolio Complete! Check out index.html to see the output');
-})
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio Complete! Check out index.html to see the output');
+// })
